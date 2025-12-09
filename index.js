5f1c8daacedd228795f67462b51166e5a12c8927
Sabin <sabintox19@gmail.com>
2025-12-09 11:02:16 +0200

    first fix on index.js

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+// const PORT = 3000;
+
+// app.use(express.static(path.join(__dirname, 'pages')));
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'pages', 'index.html'));
+// });
+
+// app.get('/about', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'pages', 'about.html'));
+// });
+
+// app.get('/contactus', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'pages', 'contactus.html'));
+// });
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+
+
 const express = require('express');
 const path = require('path');
 
@@ -18,6 +44,6 @@ app.get('/contactus', (req, res) => {
     res.sendFile(path.join(__dirname, 'pages', 'contactus.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
